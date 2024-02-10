1a71c02e96f71cbf59ef7c24954cbf4a2544cd90
Jahid Hasan <jahid0676@gmail.com>
2024-02-10 15:35:00 +0600

    Added Cart details Modal and show, hide

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -26,24 +26,28 @@ export default function MovieCard({ movie }) {
         <>
             {showModal && <MovieDetailsModal movie={movie} onClose={handleClose} />}
 
-            <a href="#" onClick={() => movieSelection(movie)}>
-                <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+            <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+                <a href="#" onClick={ () => movieSelection(movie) }>
                     <img className="w-full object-cover" src={getImageUrl(movie.cover)} alt={movie.title} />
+                </a>
+
                     <figcaption className="pt-4">
-                        <h3 className="text-xl mb-1">{movie.title}</h3>
-                        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-                        
-                        <div className="flex items-center space-x-1 mb-5">
-                            {<Rating rating={movie.rating} />}
-                        </div>
+                        <a href="#" onClick={ () => movieSelection(movie) }>
+                            <h3 className="text-xl mb-1">{movie.title}</h3>
+                            <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+                            
+                            <div className="flex items-center space-x-1 mb-5">
+                                {<Rating rating={movie.rating} />}
+                            </div>
+                        </a>
 
                         <a className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm" href="#">
                             <img src="./assets/tag.svg" alt="" />
                             <span>${movie.price} | Add to Cart</span>
                         </a>
                     </figcaption>
-                </figure>
-            </a>
+                
+            </figure>
         </>
     );
 }
